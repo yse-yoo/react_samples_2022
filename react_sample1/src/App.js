6220da4cea615eee5d66c5902607991abe6b537a
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-01-25 12:03:25 +0900

    create react app

---
+++ react_sample1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h2>React App</h2>
+        <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
