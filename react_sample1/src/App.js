dd07af99f4750b979e8fb086fccefe74c70a8684
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-02-15 11:35:50 +0900

    import Message

--- react_sample1/src/App.js
+++ react_sample1/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import './Message';
+import Message from './Message';
+
 
 function App() {
   return (
     <div classname="app">
         <h2>React App</h2>
         <p>こんにちは</p>
+
+        <Message />
     </div>
   );
 }
--- react_sample1/src/Message.js
+++ react_sample1/src/Message.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Message extends Component {
   render() {
     return (
-      <div>Message</div>
+      <div>Messageコンポーネントを読み込みました</div>
     )
   }
 }
