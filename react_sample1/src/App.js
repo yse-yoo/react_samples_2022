4a82698fa76542a7a3354168a0a945c5e2004e59
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-02-17 11:23:03 +0900

    add function component

--- react_sample1/src/App.js
+++ react_sample1/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import './Message';
 import Message from './Message';
+import ItemList from './ItemList';
 
+
+var message= "親コンポーネントからデータを受け渡し";
 
 function App() {
+
   return (
     <div classname="app">
         <h2>React App</h2>
         <p>こんにちは</p>
 
-        <Message message="親コンポーネントからデータを受け渡し" />
+        <Message message={message} />
+
+        <ItemList />
     </div>
   );
 }
---
+++ react_sample1/src/ItemList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ItemList() {
+  return (
+    <div>
+        <h2>商品一覧</h2>
+    </div>
+  )
+}
